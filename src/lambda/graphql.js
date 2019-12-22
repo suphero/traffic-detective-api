9ebb4730264c8d1de756f3a2293037a8ebfaf34c
suphero <harunsokullu@gmail.com>
2019-12-23 01:34:52 +0300

    add header support

--- src/lambda/graphql.js
+++ src/lambda/graphql.js
@@ -1,13 +1,22 @@
 const { ApolloServer } = require("apollo-server-lambda");
+const jwt = require('jsonwebtoken');
 const typeDefs = require("../helpers/typedefs");
 const resolvers = require("../helpers/resolvers");
 const connectToMongoDB = require("../helpers/db");
+const config = require("../config");
 
 exports.handler = async function(event, context) {
   const db = await connectToMongoDB();
   const server = new ApolloServer({
     typeDefs,
-    resolvers: resolvers(db)
+    resolvers: resolvers(db),
+    context: ({ event }) => {
+      const { headers } = event;
+      const token = headers.authorization || '';
+      if (!token) return;
+      const user = jwt.verify(token, config.secret);
+      return { user };
+    }
   });
   return new Promise((yay, nay) => {
     const cb = (err, args) => (err ? nay(err) : yay(args));
